b8321e9ae6f69aef0d6b6e4f62658c5719a2e036
Sebastian Orosco <sebas_ale05@hotmail.com>
2024-09-20 00:17:46 -0300

    creando class sv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+
+app.use(express.json())
+
+
+app.use('/api', require('./routes/index.routes'))
+
+
 app.listen(3001, () => {
     console.log('servidor back', 3001);
 })
