97519a4e6568c94ac684ee8f80c3f3ebe1d30130
agusmena <agusmena60@mail.com>
2024-09-23 09:17:23 -0300

    resolviendo conflictos y otros cambios

--- src/database/config.js
+++ src/database/config.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-require('dotenv').config(); 
+const mongoose = require('mongoose');
+
 try {
     mongoose.connect(process.env.MONGODB_CONNECT)
     .then(()=> console.log('Conectado a MongoDB'))
--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ require('dotenv').config()
 const Server = require('./server/config')
 const server = new Server()
 
-
 server.listen()
