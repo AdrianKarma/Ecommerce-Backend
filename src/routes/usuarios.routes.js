eebef0a8908a3b4b14cef5aa3f0dea62458f93d2
Sebastian Orosco <sebas_ale05@hotmail.com>
2024-09-29 22:49:33 -0300

    encriptacion de password y validacion al iniciar sesion

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
-const { registrarUsuario, obtenerTodosLosUsuarios, obtenerUnUsuario, bajaFisicaUsuario, bajaLogicaUsuario } = require('../controllers/usuarios.controllers')
+const { registrarUsuario, obtenerTodosLosUsuarios, obtenerUnUsuario, bajaFisicaUsuario, bajaLogicaUsuario, iniciarSesionUsuario } = require('../controllers/usuarios.controllers')
 const router = Router()
 
 
 
 router.post('/', registrarUsuario)
+router.post('/login', iniciarSesionUsuario)
 router.get('/', obtenerTodosLosUsuarios)
 router.get('/:idUsuario', obtenerUnUsuario)
 router.delete('/:idUsuario', bajaFisicaUsuario)
